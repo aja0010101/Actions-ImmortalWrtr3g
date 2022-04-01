36f3f3bb6ad232e5c684210c5eeab23c2bc625a7
kakashi <77133451+990052053@users.noreply.github.com>
2022-04-02 07:26:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add custom feeds source
+echo 'src-git aliyundrive-webdav https://github.com/messense/aliyundrive-fuse' >>feeds.conf.default

8314f04388de053eb977bde87938142a32622e78
kakashi <77133451+990052053@users.noreply.github.com>
2022-04-02 08:18:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add custom feeds source
-echo 'src-git aliyundrive https://github.com/messense/aliyundrive-fuse/tree/main/openwrt' >>feeds.conf.default
+#echo 'src-git ' >>feeds.conf.default

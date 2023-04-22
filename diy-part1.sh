4312fac6ba83375d4f37527f1f1c77becae064ca
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2023-04-22 19:08:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 
 # Add Custom feeds source
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-
-
-
+# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git n2n https://github.com/alecthw/openwrt-n2n' >>feeds.conf.default
